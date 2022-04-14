19a5177f3a8b57f6220df4626d39b8529f2eae92
jasbanza <jason.banfield.za@gmail.com>
2022-04-14 05:55:42 +0200

    fetching endpoints working

--- README.md
+++ README.md
@@ -1,16 +1,21 @@
 # cosmos-api-aggregator
 _Node.js CORS reverse-proxy REST API gateway for all chains found in https://github.com/cosmos/chain-registry_
 
+
+### Configuration
+
+***chain-registry-defaults.json*** - config file for user defaults
+
 ## Features:
 - ***A single endpoint*** for all Cosmos chains rest APIs.
 - ***Dynamically scaling*** - No need to maintain a config file when chains are added or updated in the public Cosmos chain-registry.
 - ***CORS proxy*** - Not all endpoints have CORS enabled, so web apps will need a CORS proxy.
 - ***Low maintenance*** - The only time you will need to change config, is if you want to override...
 - ***Custom override*** - If there's a preferred endpoint you'd like to use instead of the defaults.
-- ***Caching*** - Keeps cached record of endpoints found in the chain-registry. 
-- ***Error handling*** If a chain's endpoint responds with an error, the proxy will update cached endpoints and/or try secondary endpoints. 
+- ***Caching*** - Keeps cached record of endpoints found in the chain-registry.
+- ***Error handling*** If a chain's endpoint responds with an error, the proxy will update cached endpoints and/or try secondary endpoints.
 
 ## Ideas for later
-- ***Monitoring*** - See which endpoints are not working, and alert maintainer 
-- ***Logging*** - Errors are logged 
+- ***Monitoring*** - See which endpoints are not working, and alert maintainer
+- ***Logging*** - Errors are logged
 - ***Akash*** - docker compose & akash deploy
--- index.js
+++ index.js
@@ -1,8 +1,136 @@
+'use strict';
+
+const http = require("http");
+const {
+  URL,
+  URLSearchParams
+} = require('url');
+
+const fetch = require('node-fetch');
+/* Default config */
+const config = {
+  "host": 'localhost',
+  "port": 9000
+};
+const args = process.argv.slice(2);
+
 const cosmos = {};
-cosmos.test_address = "cosmos1uvay4nxlvy4uud25dcvdmruhl4n0zg0dv0sqk2";
 
+// used for raw registry response
+cosmos.chain_registry = {
+  "api_github_repotree": "https://api.github.com/repos/cosmos/chain-registry/git/trees/master",
+  "chains": {},
+  "chain_count": null,
+  "update_from_github": async function() {}
+};
+
+// TODO: make this a module
+cosmos.chain_registry.update_from_github = async function() {
+  await fetch(cosmos.chain_registry.api_github_repotree)
+    .then((res) => res.json())
+    .then((json) => {
+      let chain_count = 0;
+      json.tree.forEach((item, i) => {
+        if (item.path == ".github") return; // skip root
+        if (item.type == "tree") {
+          cosmos.chain_registry.chains[item.path] = `https://raw.githubusercontent.com/cosmos/chain-registry/master/${item.path}/chain.json`;
+          chain_count++;
+        }
+      });
+      cosmos.chain_registry.chain_count = chain_count;
+    });
+  console.log('\x1b[34m%s\x1b[0m', `config.chain_registry updated. Total chains: ${cosmos.chain_registry.chain_count}`);
+};
+cosmos.chain_registry.get_chain_json_file = async function(chainName) {
+  if (cosmos.chain_registry.chains[chainName]) {
+    let chainURL = cosmos.chain_registry.chains[chainName];
+    let chainJson = {};
+    await fetch(chainURL)
+      .then((res) => res.json())
+      .then((json) => {
+        console.dir(json, {
+          colors: true
+        });
+        chainJson = json;
+      });
+    return chainJson;
+  }
+  return false;
+};
+
+
+
+// generic functions
+async function getJSONBodyAsync(req) {
+  const buffers = [];
+  for await (const chunk of req) {
+    buffers.push(chunk);
+  }
+  const data = Buffer.concat(buffers).toString();
+  return JSON.parse(data);
+}
+
+
+// HTTP Server
+const server = http.createServer(async (req, res) => {
+  console.log('\x1b[36m%s\x1b[0m', "##### Client Request: " + req.method + " " + req.url);
+
+  const url = new URL(req.url, "http://example.test");
+
+  const res_json = {};
+  if (req.method === "GET") {
+    const arrPath = url.pathname.split("/");
+    // console.log(arrPath);
+    switch (arrPath[1]) {
+      case "chain": // https://localhost:9000/chain/{chainName}
+        if (arrPath[2] && arrPath[2] != '') {
+          console.log('\x1b[36m%s\x1b[0m', `### Chain Request: '${arrPath[2]}'`);
+          let chainName = arrPath[2];
+          let chainJson = await cosmos.chain_registry.get_chain_json_file(chainName);
+          if (chainJson) {
+            res_json.chain = chainName;
+            res_json.apis = chainJson.apis.rest;
+          } else {
+            res_json.error = "Chain not found";
+            res_json.msg = "Chain not found";
+          }
+          //set the response
+          res.writeHead(200, {
+            "Content-Type": "application/json"
+          });
+
+          // let res_json = await getJSONBodyAsync(req);
+          console.dir(res_json, {
+            colors: true
+          });
+          res.write(JSON.stringify(res_json));
+          res.end();
+          return;
+        }
+
+        break;
+      default:
+        break;
+    }
+  }
+
+
+  console.log(400);
+  res.writeHead(404);
+  res.end();
+});
+
+server.listen(config.port, config.host, () => {
+  console.log('\x1b[33m%s\x1b[0m', `Server is running on http://${config.host}:${config.port}`);
+
+  console.log(cosmos.chain_registry.update_from_github());
+});
+
+
+// REFERENCE:
+// cosmos.test_address = "cosmos1uvay4nxlvy4uud25dcvdmruhl4n0zg0dv0sqk2";
+/*
 cosmos.chain = {};
-
 cosmos.chain.getAPIUrl = async function(prefix) {
   let apiURL = "";
   let chains = {};
@@ -20,7 +148,6 @@ cosmos.chain.getAPIUrl = async function(prefix) {
   return apiURL;
 };
 
-cosmos.api = {};
 
 cosmos.api.balances = async function(address) {
 
@@ -36,3 +163,5 @@ cosmos.api.balances = async function(address) {
     });
   return balances;
 };
+
+*/
